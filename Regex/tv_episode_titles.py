4b20af7dbd059e9709a60ba1a8c31871105a91e3
Esther <e.mbanzabig@alustudent.com>
2023-05-23 16:24:57 +0200

    HACKATHON

--- Regex/tv_episode_titles.py
+++ Regex/tv_episode_titles.py
@@ -7,7 +7,7 @@ sample_string = 'Merlin Th S04E07: Last Gift'
 
 tV_episode_titles = re.compile('([A-Z]\w+\s?)+S\d{2}E\d{2}:\s([A-Z]\w+\s?)+')
 
-show_names = tv_episode_titles.finditer(sample_string)
+show_names = tV_episode_titles.finditer(sample_string)
 
 for name in show_names:
-  print(name)
+    print(name)
