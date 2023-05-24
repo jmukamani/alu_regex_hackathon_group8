3c32dc2b93eb9d839d26f7b8e59148b81e48121e
Samuel <molubode81@gmail.com>
2023-05-24 12:53:15 +0100

    Some change

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,2 +1,17 @@
 
-const userInput = document.getElementById('userInput')
+const input = document.getElementById('user-input')
+const submitButton = document.getElementById('input-form')
+
+console.log('We aare here')
+
+submitButton.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const userInput = input.value
+    input.value = ''
+
+    console.log(userInput)
+
+
+    // fetch()
+})
